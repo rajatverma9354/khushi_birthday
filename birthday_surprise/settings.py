9d9662119f923469f69057d1ea90168da70736d5
Rajat@652000 <rajatverma652000@gmail.com>
2026-07-01 00:14:46 +0530

    Fix duplicate static files

--- birthday_surprise/settings.py
+++ birthday_surprise/settings.py
@@ -132,9 +132,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-STATICFILES_DIRS = [
-    BASE_DIR / 'surprise' / 'static',
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
